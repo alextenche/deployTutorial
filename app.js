e8fb6b717413672de739c7449751442c2e0dbb6f
Alexandru Tenche <alex.tenche@gmail.com>
2016-07-28 23:31:15 +0300

    removed hostaname from app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 
 var http = require('http');
 
-var hostname = '127.0.0.1';
+// var hostname = '127.0.0.1';
 var port = process.env.PORT || 1337;
 
 http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('app deployed !\n');
-}).listen(port, hostname, function(){
-  console.log('server running at http://' + hostname + ":" + port);
+}).listen(port, function(){
+  console.log('server running at http://deploy_tutorial:' + port);
 });
